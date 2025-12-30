16dbfe3baac0000df55b5c0f056914205c6dc40a
Lion King <86064418+Lion1414@users.noreply.github.com>
2025-12-30 18:44:39 -0500

    Updated ValueProps.tsx

--- src/components/ValueProps.tsx
+++ src/components/ValueProps.tsx
@@ -22,7 +22,7 @@ const valueItems = [
 
 const ValueProps: React.FC = () => {
   return (
-    <section id="why-closio" className="py-16 sm:py-20 md:py-28 lg:py-32 bg-[#0A1628]">
+    <section id="why-closio" className="py-16 sm:py-20 md:py-28 lg:py-32 bg-black">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="grid gap-12 lg:gap-16 grid-cols-1 lg:grid-cols-[1.3fr_1fr] items-center">
 
