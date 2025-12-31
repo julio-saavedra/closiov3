de265ffb4cef0948db9b33cfb62c34809178632e
Lion King <86064418+Lion1414@users.noreply.github.com>
2025-12-30 22:59:48 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,7 +61,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M -100 130 L 100 130 Q 170 130, 170 200 L 170 530 Q 170 600, 240 600 L 650 600"
+        d="M -100 130 L 100 130 Q 170 130, 170 200 L 170 530 Q 170 600, 240 600 L 400 600 Q 470 600, 470 670 L 470 800"
         fill="none"
         stroke="rgba(255, 255, 255, 0.15)"
         strokeWidth="32"
@@ -70,7 +70,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M -100 130 L 100 130 Q 170 130, 170 200 L 170 530 Q 170 600, 240 600 L 650 600"
+        d="M -100 130 L 100 130 Q 170 130, 170 200 L 170 530 Q 170 600, 240 600 L 400 600 Q 470 600, 470 670 L 470 800"
         fill="none"
         stroke="white"
         strokeWidth="16"
@@ -118,7 +118,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M -100 10 L 400 10 Q 470 10, 470 80 L 470 530 Q 470 600, 540 600 L 650 600"
+        d="M -100 10 L 400 10 Q 470 10, 470 80 L 470 600"
         fill="none"
         stroke="rgba(255, 255, 255, 0.15)"
         strokeWidth="32"
@@ -127,7 +127,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M -100 10 L 400 10 Q 470 10, 470 80 L 470 530 Q 470 600, 540 600 L 650 600"
+        d="M -100 10 L 400 10 Q 470 10, 470 80 L 470 600"
         fill="none"
         stroke="white"
         strokeWidth="16"
